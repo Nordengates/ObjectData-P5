c64d77d4d53945923ea0deb5b8339b3069134a27
supermusta <162694128+supermusta@users.noreply.github.com>
2024-05-18 23:13:18 +0200

    se modifica el formulario para mostrar Inscripciones por fechas

--- ObjectData/src/main/java/ObjectData_app/ObjectData_controller/InscripcionController.java
+++ ObjectData/src/main/java/ObjectData_app/ObjectData_controller/InscripcionController.java
@@ -12,6 +12,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -20,18 +21,64 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import ObjectData_app.ObjectData_model.ExcursionModel;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
 public class InscripcionController {
-     @FXML
+    @FXML
+    private Button bnBuscarMostrarInscripcion;
+
+    @FXML
+    private DatePicker tfFechaInicioinscripcion;
+
+    @FXML
+    private DatePicker tfFechaFinInscripcion;
+
+    @FXML
+    private TableView<InscripcionModel> taResultadoInscripcion;
+
+    @FXML
+    private TableColumn<InscripcionModel, Integer> taNumeroInscripcion;
+
+    @FXML
+    private TableColumn<InscripcionModel, Integer> taNumeroSocio;
+
+    @FXML
+    private TableColumn<InscripcionModel, Integer> taNumeroExcursion;
+
+    @FXML
+    private TableColumn<InscripcionModel, Date> taFecha;
+
+    @FXML
+    private TableColumn<InscripcionModel, String> taNombreExcursion;
+
+    @FXML
+    private TableColumn<InscripcionModel, String> taNombreSocio;
+
+    @FXML
+    private TableColumn<InscripcionModel, Double> taPrecio;
+
+    @FXML
+    private TableColumn<InscripcionModel, Double> taPrecioTotal;
+
+    @FXML
+    private TableColumn<InscripcionModel, String> taDescuento;
+
+    @FXML
+    private Text tInfo;
+
+    @FXML
     private BorderPane mainContainer;
+
     @FXML
     private Label label;
 
@@ -57,10 +104,10 @@ public class InscripcionController {
     private TableView<SocioEstandarModel> taSocios;
 
     @FXML
-    private TableColumn<SocioEstandarModel,Integer> taN;
+    private TableColumn<SocioEstandarModel, Integer> taN;
 
     @FXML
-    private TableColumn<SocioEstandarModel,String> taD;
+    private TableColumn<SocioEstandarModel, String> taD;
 
     @FXML
     private TableView<ExcursionModel> taExcursion;
@@ -70,8 +117,7 @@ public class InscripcionController {
 
     @FXML
     private TableColumn<ExcursionModel, String> taDesc;
-
-    // Se inicializan las vistas necasias.
+// Se inicializan las vistas necasias.
 
     // Metodo para crear ID de inscripcion dinamicos
     public static int generarID() {
@@ -248,7 +294,8 @@ private void handleCrearInscripcion(ActionEvent event) {
         }
     }
 
-    public static void mostrarInscripcionPorSocio() {
+    public static void mostrarInscripcionPorSocio() 
+    {
         String[] retorno = InscView.formFiltrarPorSocio();
         if (retorno != null && retorno.length > 0) {
             String numSocio = retorno[0];
@@ -268,73 +315,86 @@ private void handleCrearInscripcion(ActionEvent event) {
         }
     }
 
-    public static void eliminarInscripcion() {
+    public static void eliminarInscripcion() 
+    {
         String listadoInscripciones = "";
         boolean inscripcionEliminada = false;
 
-        try {
-            listadoInscripciones = InscripcionModel.obtenerListadoInscripciones();
-        } catch (Exception e) {
-            RespView.excepcionesControllerView(e.getMessage());
-        }
-        RespView.respuestaControllerView(listadoInscripciones);
-        String retorno = InscView.formEliminarInscripcionView(listadoInscripciones);
-        int num;
+        //try {
+          //  listadoInscripciones = InscripcionModel.obtenerListadoInscripciones();
+        //} catch (Exception e) {
+     //       RespView.excepcionesControllerView(e.getMessage());
+        //}
+       // RespView.respuestaControllerView(listadoInscripciones);
+        //String retorno = InscView.formEliminarInscripcionView(listadoInscripciones);
+       // int num;
         // Manejar el caso en que el usuario no ingrese un número válido
-        if (retorno.matches("\\d+")) {
-            num = Integer.parseInt(retorno);
-        } else {
-            RespView.excepcionesControllerView("El número de inscripción ingresado no es válido.");
+       // if (retorno.matches("\\d+")) {
+         //   num = Integer.parseInt(retorno);
+       // } else {
+         //   RespView.excepcionesControllerView("El número de inscripción ingresado no es válido.");
+        //    return;
+        //}
+
+        //try {
+          //  inscripcionEliminada = InscripcionModel.eliminarInscripcionNumero(num);
+        //} catch (Exception e) {
+            //RespView.excepcionesControllerView(e.getMessage());
+        //}
+
+        //if (inscripcionEliminada) {
+            //RespView.respuestaControllerView("La inscripción ha sido eliminada exitosamente.");
+        //} else {
+            //RespView.excepcionesControllerView(
+          //          "No se pudo eliminar la inscripción. Verifique el número de inscripción y asegúrese de que la fecha de inscripción sea anterior a la fecha de la excursión.");
+        //}
+    
+    }
+
+    public void mostrarInscripcionPorFecha() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+        Date fechaInicio = null, fechaFin = null;
+
+        try {
+            fechaInicio = java.sql.Date.valueOf(tfFechaInicioinscripcion.getValue());
+            fechaFin = java.sql.Date.valueOf(tfFechaFinInscripcion.getValue());
+        } catch (Exception e) {
+            NotificacionView.Notificacion("ERROR", "Error al obtener las fechas de filtrado", "Ha ocurrido un problema al intentar obtener las fechas de filtrado.");
             return;
         }
 
-        try {
-            inscripcionEliminada = InscripcionModel.eliminarInscripcionNumero(num);
-        } catch (Exception e) {
-            RespView.excepcionesControllerView(e.getMessage());
+        if (fechaInicio.after(fechaFin)) {
+            NotificacionView.Notificacion("ERROR", "La fecha de inicio debe ser anterior a la fecha de fin.", "Ha ocurrido un problema al intentar obtener las fechas de filtrado.");
+            return;
         }
 
-        if (inscripcionEliminada) {
-            RespView.respuestaControllerView("La inscripción ha sido eliminada exitosamente.");
-        } else {
-            RespView.excepcionesControllerView(
-                    "No se pudo eliminar la inscripción. Verifique el número de inscripción y asegúrese de que la fecha de inscripción sea anterior a la fecha de la excursión.");
-        }
+        
+        taNumeroInscripcion.setCellValueFactory(new PropertyValueFactory<>("numeroInscripcion"));
+        taNumeroSocio.setCellValueFactory(new PropertyValueFactory<>("numeroSocio"));
+        taNumeroExcursion.setCellValueFactory(new PropertyValueFactory<>("numeroExcursion"));
+        taFecha.setCellValueFactory(new PropertyValueFactory<>("fechaInscripcion"));
+        taResultadoInscripcion.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+            if (newSelection != null) {
+                Integer contenidoCelda = newSelection.getNumeroExcursion();
+                final Clipboard clipboard = Clipboard.getSystemClipboard();
+                final ClipboardContent content = new ClipboardContent();
+                content.putString(contenidoCelda.toString());
+                clipboard.setContent(content);
+                NotificacionView.Notificacion("INFORMATION", "Copiado al portapapeles","Se copio al portapapeles el número de excursión: " + contenidoCelda);
+            }
+        });
+            // Supongamos que tienes dos variables para almacenar las fechas seleccionadas
+        
+// Verificar si las fechas seleccionadas no son nulas
+ if (fechaInicio != null && fechaFin != null) {
+        // Supongamos que listarInscripcionesFecha retorna una lista de inscripciones
+        ArrayList<InscripcionModel> inscripciones = InscripcionModel.objetoListaInscripcion(fechaInicio, fechaFin);
+        
+        // Convertir la lista a un ObservableList si es necesario
+        ObservableList<InscripcionModel> observableInscripciones = FXCollections.observableArrayList(inscripciones);
+        
+        // Establecer los items de la tabla
+        taResultadoInscripcion.setItems(observableInscripciones);
     }
-
-    public static void mostrarInscripcionPorFecha() {
-        String[] inscripciones = null;
-        String[] retorno = InscView.formFiltrarPorFechas();
-        if (retorno != null && retorno.length == 2) {
-            String fechaInicio = retorno[0];
-            String fechaFin = retorno[1];
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
-            Date fechaI = null, fechaF = null;
-            if (!fechaInicio.matches("\\d{4}/\\d{2}/\\d{2}") || !fechaFin.matches("\\d{4}/\\d{2}/\\d{2}")) {
-                RespView.excepcionesControllerView("Formato de fecha inválido. Se esperaba yyyy/MM/dd.");
-            } else {
-                try { // Parsear las fechas de inicio y fin
-                    fechaI = sdf.parse(fechaInicio);
-                    fechaF = sdf.parse(fechaFin);
-                } catch (ParseException e) {
-                    RespView.excepcionesControllerView("No se han podido parsear las fechas.");
-                }
-            }
-
-            try {
-                inscripciones = InscripcionModel.listarInscripcionesFecha(fechaI, fechaF);
-            } catch (Exception e) {
-                RespView.excepcionesControllerView("No se ha podido listar las Inscripciones.");
-
-            }
-            if (inscripciones.length > 0) {
-                RespView.respuestaControllerView("Listado de inscripciones por rango de fechas: " + inscripciones[0]);
-            } else {
-                RespView.excepcionesControllerView(
-                        "No se encontraron inscripciones para el rango de fechas especificado.");
-            }
-        } else {
-            RespView.excepcionesControllerView("Problema al obtener las fechas de filtrado.");
-        }
-    }
+}
 }
